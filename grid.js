15254889af9de1fe880817de5f071cccd13c7c3f
Oussama ALAMI <oussama.alami@um5r.ac.ma>
2021-03-10 16:19:21 +0100

    Fix some glitches

--- grid.js
+++ grid.js
@@ -1,12 +1,12 @@
-const grid_width = 20;
-const grid_height = 50;
-export const gameBoard = document.getElementById('game-board');
+const grid_width = 20
+const grid_height = 50
+export const gameBoard = document.getElementById('game-board')
 
 export function randomGridPosition() {
   return {
     x: Math.floor(Math.random() * grid_height) + 1,
     y: Math.floor(Math.random() * grid_width) + 1,
-  };
+  }
 }
 export function outsideGrid(position) {
   return (
@@ -14,5 +14,5 @@ export function outsideGrid(position) {
     position.x > grid_height ||
     position.y < 1 ||
     position.y > grid_width
-  );
+  )
 }
